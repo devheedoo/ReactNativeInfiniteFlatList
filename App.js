e1e22d7b036546ad1a61b37789d8a9bfd067f26f
Heedo Kim <devheedoo@gmail.com>
2019-09-11 17:08:45 +0900

    Refactoring

--- App.js
+++ App.js
@@ -32,41 +32,31 @@ class App extends Component {
   }
 
   makeRemoteRequest = () => {
-    const { page, seed } = this.state;
+    const { page, seed, data } = this.state;
     const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
     this.setState({ loading: true });
-    // setTimeout(() => {
-      fetch(url)
-        .then(res => res.json())
-        .then(res => {
-          if (this.state.refreshing) {
-            this.setState({
-              data: res.results,
-              error: res.error || null,
-              loading: false,
-              refreshing: false,
-            });
-          } else {
-            this.setState({
-              data: [...this.state.data, ...res.results],
-              error: res.error || null,
-              loading: false,
-              refreshing: false,
-            });
-          }
-        })
-        .catch(error => {
-          this.setState({ error, loading: false, refreshing: false });
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          data: page === 1 ? res.results : [...data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false,
         });
-    // }, 1500);
+      })
+      .catch(error => {
+        this.setState({ error, loading: false, refreshing: false });
+      });
   }
 
   handleRefresh = () => {
+    const { seed } = this.state;
     this.setState(
       {
         page: 1,
         refreshing: true,
-        seed: this.state.seed + 1,
+        seed: seed + 1,
       }, () => {
         this.makeRemoteRequest();
       }
@@ -74,9 +64,10 @@ class App extends Component {
   };
 
   handleLoadMore = () => {
+    const { page } = this.state;
     this.setState(
       {
-        page: this.state.page + 1,
+        page: page + 1,
       }, () => {
         this.makeRemoteRequest();
       }
@@ -84,12 +75,14 @@ class App extends Component {
   };
 
   render() {
+    const { data, refreshing } = this.state;
+
     return (
         <FlatList
           ListHeaderComponent={() => {
             return <View style={styles.side}><Text style={[styles.center, styles.title]}>List Header Component</Text></View>;
           }}
-          data={this.state.data}
+          data={data}
           renderItem={ ({ item, index }) => (
             <View style={styles.item}>
               <Text style={styles.title}>{`${item.name.first} ${item.name.last} ${index + 1}`}</Text>
@@ -100,7 +93,7 @@ class App extends Component {
             return <View style={styles.side}><Text style={[styles.center, styles.title]}>List Footer Component</Text></View>
           }}
           keyExtractor={item => item.email}
-          refreshing={this.state.refreshing}
+          refreshing={refreshing}
           onRefresh={this.handleRefresh}
           onEndReached={this.handleLoadMore}
           onEndReachedThreshold={0}
