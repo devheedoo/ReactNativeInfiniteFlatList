4ecc35719c7c9d6f1f34e25a433564a8f298a94c
Heedo Kim <devheedoo@gmail.com>
2019-09-11 16:15:41 +0900

    초기 레이아웃 및 더미 데이터 설정

--- App.js
+++ App.js
@@ -6,109 +6,66 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  FlatList,
 } from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+      <FlatList
+        ListHeaderComponent={() => {
+          return <View style={styles.side}><Text style={[styles.center, styles.title]}>List Header Component</Text></View>;
+        }}
+        data={[
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+          { name: { first: 'Choco', last: 'Kim' }, email: 'choco@wavetogether.com' },
+        ]}
+        renderItem={ ({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.title}>{`${item.name.first} ${item.name.last}`}</Text>
+            <Text>{item.email}</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+        )}
+        ListFooterComponent={() => {
+          return <View style={styles.side}><Text style={[styles.center, styles.title]}>List Footer Component</Text></View>
+        }}
+      />
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  center: {
+    textAlign: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  side: {
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  body: {
-    backgroundColor: Colors.white,
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  title: {
+    fontSize: 32,
   },
 });
+
 
 export default App;
